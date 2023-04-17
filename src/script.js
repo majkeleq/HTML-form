ac72bd831549ab36643a90573b763664b286e682
majkeleq <graf3rpl@gmail.com>
2023-04-17 20:10:36 +0200

    Added comments in .js

--- src/script.js
+++ src/script.js
@@ -1,8 +1,10 @@
 const formInputs = document.querySelectorAll("input");
 formInputs.forEach( function (currentValue) {
+    //Check if input value is in localStorage - if yes, override its value on page
     if (localStorage.getItem(currentValue.name) != null) {
         currentValue.value = localStorage.getItem(currentValue.name);
     }
+    //Update localStorage if user inputs sth
     currentValue.addEventListener("input", function () {
         localStorage.setItem(currentValue.name, currentValue.value);
     });
