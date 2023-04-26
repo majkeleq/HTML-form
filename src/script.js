da5c30d521188544be5c9bc5dd15d6a0ac057e22
majkeleq <graf3rpl@gmail.com>
2023-04-26 20:53:18 +0200

    Added history.html and href to navigation tabs that directs to history site. Button submits saves all fields to JSON object in localStorage. History.js creates div and inserts paragraphs with labels and its values. To be added: -submit do not override JSON objects in localStorage but append values to it

--- src/script.js
+++ src/script.js
@@ -1,11 +1,60 @@
-const formInputs = document.querySelectorAll("input");
-formInputs.forEach( function (currentValue) {
-    //Check if input value is in localStorage - if yes, override its value on page
-    if (localStorage.getItem(currentValue.name) != null) {
-        currentValue.value = localStorage.getItem(currentValue.name);
-    }
-    //Update localStorage if user inputs sth
-    currentValue.addEventListener("input", function () {
-        localStorage.setItem(currentValue.name, currentValue.value);
-    });
+//Assigning inputs to variables
+let firstName = document.getElementById("first-name");
+let lastName = document.getElementById("last-name");
+let email = document.getElementById("email");
+let phone = document.getElementById("phone");
+let company = document.getElementById("company");
+let address = document.getElementById("address");
+let submit = document.getElementById("submit-button")
+
+//Assigning values from localStorage to inputs
+if (localStorage.getItem("first-name") != null) {
+    firstName.value = JSON.parse(localStorage.getItem("first-name")).firstName;
+}
+if (localStorage.getItem("last-name") != null) {
+    lastName.value = JSON.parse(localStorage.getItem("last-name")).lastName;
+}
+if (localStorage.getItem("email") != null) {
+    email.value = JSON.parse(localStorage.getItem("email")).email;
+}
+if (localStorage.getItem("phone") != null) {
+    phone.value = JSON.parse(localStorage.getItem("phone")).phone;
+}
+if (localStorage.getItem("company") != null) {
+    company.value = JSON.parse(localStorage.getItem("company")).company;
+}
+if (localStorage.getItem("address") != null) {
+    address.value = JSON.parse(localStorage.getItem("address")).address;
+}
+
+//Adding listeners to overwrite localStorage from inputs
+firstName.addEventListener("keyup", function () {
+    localStorage.setItem("first-name", JSON.stringify({firstName: document.getElementById("first-name").value}));
+});
+lastName.addEventListener("keyup", function () {
+    localStorage.setItem("last-name", JSON.stringify({lastName: document.getElementById("last-name").value}));
+});
+email.addEventListener("keyup", function () {
+    localStorage.setItem("email", JSON.stringify({email: document.getElementById("email").value}));
+});
+phone.addEventListener("keyup", function () {
+    localStorage.setItem("phone", JSON.stringify({phone: document.getElementById("phone").value}));
+});
+company.addEventListener("keyup", function () {
+    localStorage.setItem("company", JSON.stringify({company: document.getElementById("company").value}));
+});
+address.addEventListener("keyup", function () {
+    localStorage.setItem("address", JSON.stringify({address: document.getElementById("address").value}));
+});
+submit.addEventListener("click", function () {
+    let form = {
+        firstName: document.getElementById("first-name").value,
+        lastName: document.getElementById("last-name").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        company: document.getElementById("company").value,
+        address: document.getElementById("address").value
+    };
+    localStorage.setItem("form", JSON.stringify(form));
+
 });
