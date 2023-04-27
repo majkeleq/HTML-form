4615d1b3ffcbc79740c89a7d0a77762bf4f3ddf5
majkeleq <graf3rpl@gmail.com>
2023-04-27 20:02:50 +0200

    Added printing multiple forms to history.html

---
+++ src/history.js
@@ -0,0 +1,58 @@
+//Find forms <div>
+const divForms = document.getElementById('forms');
+
+
+/*
+let firstNameLabel = document.createElement("p");
+firstNameLabel.setAttribute("class", "card-label");
+firstNameLabel.appendChild(document.createTextNode("First name"));
+
+let firstName = document.createElement("p");
+firstName.setAttribute("class", "card-first-name");
+firstName.appendChild(document.createTextNode(JSON.parse(localStorage.getItem("form")).firstName));
+
+divElement.appendChild(firstNameLabel);
+divElement.appendChild(firstName);
+divForms.appendChild(divElement);
+*/
+if (localStorage.getItem('forms').length > 0) {
+    JSON.parse(localStorage.getItem("forms")).forEach((singleForm, index) => {
+        addDiv(singleForm, index);
+    })
+
+    function addDiv(singleForm, index) {
+        //Create form <div>
+        const divElement = document.createElement('div');
+        divElement.setAttribute('class', 'card');
+        divElement.setAttribute('id', 'card' + index);
+
+        addParagraph(singleForm, "firstName", "card-first-name", 'First name', divElement);
+        addParagraph(singleForm, "lastName", 'card-last-name', 'Last name', divElement);
+        addParagraph(singleForm, "email", 'card-email', 'Email', divElement);
+        addParagraph(singleForm, "phone", 'card-phone', 'Last name', divElement);
+        addParagraph(singleForm, "company", 'card-company', 'Company', divElement);
+        addParagraph(singleForm, "address", 'card-address', 'Address', divElement);
+
+        divForms.appendChild(divElement);
+    }
+
+}
+
+function addParagraph(singleForm, att, className, labelText, divElement) {
+
+    let label = document.createElement("p");
+    label.setAttribute('class', 'card-label');
+    label.appendChild(document.createTextNode(labelText));
+
+    let paragraph = document.createElement("p");
+    //paragraph.appendChild(document.createTextNode(JSON.parse(localStorage.getItem("form"))[att]))
+    paragraph.appendChild(document.createTextNode(singleForm[att]));
+    paragraph.setAttribute("class", className);
+    divElement.appendChild(label);
+    divElement.appendChild(paragraph);
+}
+
+
+console.log(JSON.parse(localStorage.getItem("forms")));
+console.log(localStorage.getItem("forms").length);
+console.log(JSON.parse(localStorage.getItem("forms"))[0]);
